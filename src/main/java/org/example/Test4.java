8f43456eaad220c5c1cac269d69e79bb5ea5477c
YuliyaV <y.veyukova@gmail.com>
2022-02-07 21:39:07 +0300

    Lesson3

---
+++ src/main/java/org/example/Test4.java
@@ -0,0 +1,29 @@
+package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test4 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("disable-popup-blocking");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://monro24.ru");
+
+        WebElement webElement = driver.findElement(By.xpath(" //span[text()=\"Детям\"]/parent::a"));
+        webElement.click();
+        driver.quit();
+
+
+    }
+}
