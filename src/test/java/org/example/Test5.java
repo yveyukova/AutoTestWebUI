4b669021f916160935ad6b7a35caa7e666a9252f
YuliyaV <y.veyukova@gmail.com>
2022-02-27 20:42:01 +0300

    lesson7

--- src/test/java/org/example/Test5.java
+++ src/test/java/org/example/Test5.java
@@ -1,6 +1,8 @@
 package org.example;
+import io.qameta.allure.*;
+import org.example.utils.Monro24Page;
 import org.junit.jupiter.api.*;
-
+@Story("Тестируем заказ на сайте monro24.ru")
 public class Test5 {
     static Monro24Page monro24Page;
     @BeforeAll
@@ -15,16 +17,22 @@ public class Test5 {
     }
     @AfterEach
     void getPage() {
+        monro24Page.printBrowserLog();
         monro24Page.getRoot();
     }
 
     @Test
+    @DisplayName("Тест оформления заказа")
+    @Description("Проверка оформления заказа (добавления товара в корзину)")
+    @Link("https://monro24.ru")
+    @Severity(SeverityLevel.MINOR)
     void test() {
         Assertions.assertTrue(
                 monro24Page.getCatalog()
                         .clickModel()
                         .clickSize()
                         .clickAddBtn()
+                        .makeScreenshot()
                         .getOrderBtn()
                         .equals("Оформить заказ"));
     }
--- src/test/java/org/example/Test6.java
+++ src/test/java/org/example/Test6.java
@@ -1,8 +1,10 @@
 package org.example;
+import io.qameta.allure.*;
+import org.example.utils.Monro24Page;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
+@Story("Тестируем работу формы поддержки на сайте monro24.ru")
 public class Test6 {
     static Monro24Page monro24Page;
     @BeforeAll
@@ -17,10 +19,15 @@ public class Test6 {
     }
     @AfterEach
     void getPage() {
+        monro24Page.printBrowserLog();
         monro24Page.getRoot();
     }
     @ParameterizedTest
     @CsvSource({"Тест, +79991234567, Проверка. Перезванивать не нужно"})
+    @DisplayName("Тест кнопки Заказать звонок")
+    @Description("Проверка работы кнопки Заказать звонок и заполнения обратной связи")
+    @Link("https://monro24.ru")
+    @Severity(SeverityLevel.MINOR)
     void test(String name, String phone, String mess) {
         Assertions.assertTrue(
                 monro24Page.getRoot()
@@ -29,6 +36,7 @@ public class Test6 {
                         .sendSupportTel(phone)
                         .sendSupportMsg(mess)
                         .clickSupportBtn()
+                        .makeScreenshot()
                         .getHelp()
                         .equals("Помощь"));
     }
